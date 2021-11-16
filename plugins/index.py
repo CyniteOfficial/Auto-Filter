9271917fc369b42cb589283f8069c5d67bd384bf
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2021-11-16 20:47:25 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -30,7 +30,7 @@ async def index_files(bot, query):
         return await query.answer('Wait until previous process complete.', show_alert=True)
     msg = query.message
 
-    await query.answer('Processing...⏳', show_alert=True)
+    await query.answer('𝗖𝗵𝗲𝗰𝗸𝗶𝗻𝗴...⏳😜', show_alert=True)
     if int(from_user) not in ADMINS:
         await bot.send_message(int(from_user),
                                f'Your Submission for indexing {chat} has been accepted by our moderators and will be added soon.',
@@ -83,11 +83,11 @@ async def send_for_index(bot, message):
     if message.from_user.id in ADMINS:
         buttons = [
             [
-                InlineKeyboardButton('Yes',
+                InlineKeyboardButton('𝗬𝗲𝘀',
                                      callback_data=f'index#accept#{chat_id}#{last_msg_id}#{message.from_user.id}')
             ],
             [
-                InlineKeyboardButton('close', callback_data='close_data'),
+                InlineKeyboardButton('𝗖𝗹𝗼𝘀𝗲', callback_data='close_data'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -104,11 +104,11 @@ async def send_for_index(bot, message):
         link = f"@{message.forward_from_chat.username}"
     buttons = [
         [
-            InlineKeyboardButton('Accept Index',
+            InlineKeyboardButton('😍 𝗔𝗰𝗰𝗲𝗽𝘁 𝗜𝗻𝗱𝗲𝘅 😍',
                                  callback_data=f'index#accept#{chat_id}#{last_msg_id}#{message.from_user.id}')
         ],
         [
-            InlineKeyboardButton('Reject Index',
+            InlineKeyboardButton('😥 𝗥𝗲𝗷𝗲𝗰𝘁 𝗜𝗻𝗱𝗲𝘅 😥',
                                  callback_data=f'index#reject#{chat_id}#{message.message_id}#{message.from_user.id}'),
         ]
     ]
