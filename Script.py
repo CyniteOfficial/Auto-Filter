801864ee102a80c31d2ddc06f2595c70e1732a73
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2021-11-29 17:12:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -25,10 +25,10 @@ class script(object):
 3. alert buttons have a limit of 64 characters.
 
 <b>Commands and Usage:</b>
-➜ /filter - <code>add a filter in chat</code>
-➜ /filters - <code>list all the filters of a chat</code>
-➜ /del - <code>delete a specific filter in chat</code>
-➜ /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+➾ /filter - <code>add a filter in chat</code>
+➾ /filters - <code>list all the filters of a chat</code>
+➾ /del - <code>delete a specific filter in chat</code>
+➾ /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
 - ᗩᒍᗩ᙭ Supports both url and alert inline buttons.
@@ -60,47 +60,47 @@ class script(object):
 2. Send <code>/connect</code> for connecting me to ur PM
 
 <b>Commands and Usage:</b>
-➜ /connect  - <code>connect a particular chat to your PM</code>
-➜ /disconnect  - <code>disconnect from a chat</code>
-➜ /connections - <code>list all your connections</code>"""
+➾ /connect  - <code>connect a particular chat to your PM</code>
+➾ /disconnect  - <code>disconnect from a chat</code>
+➾ /connections - <code>list all your connections</code>"""
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
 These are the extra features of ᗩᒍᗩ᙭
 
 <b>Commands and Usage:</b>
-➜ /id - <code>get id of a specifed user.</code>
-➜ /info  - <code>get information about a user.</code>
-➜ /imdb  - <code>get the film information from IMDb source.</code>
-➜ /search  - <code>get the film information from various sources.</code>"""
+➾ /id - <code>get id of a specifed user.</code>
+➾ /info  - <code>get information about a user.</code>
+➾ /imdb  - <code>get the film information from IMDb source.</code>
+➾ /search  - <code>get the film information from various sources.</code>"""
     ADMIN_TXT = """Help: <b>Admin mods</b>
 
 <b>NOTE:</b>
 This module only works for my OᗯᑎEᖇ⚡
 
 <b>Commands and Usage:</b>
-➜ /logs - <code>to get the rescent errors</code>
-➜ /stats - <code>to get status of files in db.</code>
-➜ /delete - <code>to delete a specific file from db.</code>
-➜ /users - <code>to get list of my users and ids.</code>
-➜ /chats - <code>to get list of the my chats and ids </code>
-➜ /leave  - <code>to leave from a chat.</code>
-➜ /disable  -  <code>do disable a chat.</code>
-➜ /ban  - <code>to ban a user.</code>
-➜ /unban  - <code>to unban a user.</code>
-➜ /channel - <code>to get list of total connected channels</code>
-➜ /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """✯ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+➾ /logs - <code>to get the rescent errors</code>
+➾ /stats - <code>to get status of files in db.</code>
+➾ /delete - <code>to delete a specific file from db.</code>
+➾ /users - <code>to get list of my users and ids.</code>
+➾ /chats - <code>to get list of the my chats and ids </code>
+➾ /leave  - <code>to leave from a chat.</code>
+➾ /disable  -  <code>do disable a chat.</code>
+➾ /ban  - <code>to ban a user.</code>
+➾ /unban  - <code>to unban a user.</code>
+➾ /channel - <code>to get list of total connected channels</code>
+➾ /broadcast - <code>to broadcast a message to all users</code>"""
+    STATUS_TXT = """✮ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
 ✮ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
 ✮ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
 ✮ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
 ✮ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
-Group = {}(<code>{}</code>)
-Total Members = <code>{}</code>
-Added By - {}
+𝙶𝚁𝙾𝚄𝙿 = {}(<code>{}</code>)
+𝚃𝙾𝚃𝙰𝙻 Members = <code>{}</code>
+𝙰𝙳𝙳𝙴𝙳 𝙱𝚈 - {}
 """
     LOG_TEXT_P = """#NewUser
-ID - <code>{}</code>
-Name - {}
+𝙸𝙳 - <code>{}</code>
+𝙽𝙰𝙼𝙴 - {}
 """
