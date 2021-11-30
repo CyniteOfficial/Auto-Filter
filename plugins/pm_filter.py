b8f824b7daae9befbb2a206c657804795e12b4ac
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2021-11-30 23:57:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -612,7 +612,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}] ›› {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
