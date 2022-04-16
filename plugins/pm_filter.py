5a88f3b0fc54e8cc26b7c0ab49a9f1e577bbaa8d
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-04-16 16:49:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -621,8 +621,6 @@ async def auto_filter(client, msg, spoll=False):
     imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
     if imdb:
         cap = IMDB_TEMPLATE.format(
-            group = message.chat.title,
-            requested = message.from_user.mention,
             query = search,
             title = imdb['title'],
             votes = imdb['votes'],
