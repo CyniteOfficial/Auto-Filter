9c7020d2c0eb089dcc20328c50164a4d9d1597ec
technicalaks <105633032+technicalaks@users.noreply.github.com>
2022-05-22 14:31:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a> Iam a powerful autofilter bot with some more featues.. ❤💫💫💫"""
+    START_TXT = """HELLO {},
+My Name Is <a href=https://t.me/{}>{}</a> I am a powerful autofilter bot with some more featues.. ❤💫💫💫"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✮ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
