ab04375c737c549f7f9457c97d9ee15b99f2cb46
technicalaks <105633032+technicalaks@users.noreply.github.com>
2022-05-24 19:19:14 +0530

    Update carbon.py

--- plugins/modules/carbon.py
+++ plugins/modules/carbon.py
@@ -5,10 +5,10 @@ from plugins.function import make_carbon
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 aiohttpsession = ClientSession()
 
-C = "**𝙼𝙰𝙳𝙴 𝙱𝚈 [𝙰𝙹𝙰𝚇](https://t.me/Devil0Bot_Bot)**"
+C = "**𝙼𝙰𝙳𝙴 𝙱𝚈 [AYUSH](https://t.me/Aks_support01_bot)**"
 F = InlineKeyboardMarkup(
 [[
-     InlineKeyboardButton("𝚄𝙿𝙳𝙰𝚃𝙴𝚉 𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url="https://t.me/OpusTechz")
+     InlineKeyboardButton("𝚄𝙿𝙳𝙰𝚃𝙴𝚉 𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url="https://t.me/Imdb_updates")
 ]]
 )
 
